3e720eaaeaadf42b8a87a6980800951a7d6a74d1
yimeng <513316336@qq.com>
2017-11-30 20:02:53 +0800

    ueditor 前后端分离

--- src/main/java/priv/yimeng/GradleDemoApplication.java
+++ src/main/java/priv/yimeng/GradleDemoApplication.java
@@ -2,8 +2,10 @@ package priv.yimeng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class GradleDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/priv/yimeng/demo/servlet/UeServlet.java
@@ -0,0 +1,41 @@
+package priv.yimeng.demo.servlet;
+
+import com.baidu.ueditor.ActionEnter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Description: ${DESCRIPTION}
+ * CreateDate:  2017-11-30
+ *
+ * @author yimeng
+ * @version 1.0
+ */
+@WebServlet(urlPatterns = "/ue", name = "ue")
+public class UeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String rootPath = req.getServletContext().getRealPath("/");
+        String configFileName = UeServlet.class.getClassLoader().getResource("config.json").getPath();
+        PrintWriter writer = resp.getWriter();
+        resp.setCharacterEncoding("UTF-8");
+        writer.write(new ActionEnter(req, rootPath, configFileName).exec());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}

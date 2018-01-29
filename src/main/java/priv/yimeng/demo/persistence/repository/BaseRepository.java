fe46a1805264bd904fe2a4e008bb2f8adc6a4b76
yimeng <513316336@qq.com>
2018-01-29 18:37:03 +0800

    list method

--- src/main/java/priv/yimeng/demo/persistence/repository/BaseRepository.java
+++ src/main/java/priv/yimeng/demo/persistence/repository/BaseRepository.java
@@ -3,8 +3,11 @@ package priv.yimeng.demo.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.transaction.annotation.Transactional;
+import priv.yimeng.demo.persistence.Filter;
+import priv.yimeng.demo.persistence.Order;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Desc
@@ -22,5 +25,24 @@ public interface BaseRepository<T, ID extends Serializable> extends JpaRepositor
     void persist(T t);
 
     T getOneObject(ID username);
+
+    /**
+     * 依据filter查询实体集合
+     *
+     * @param filters 条件
+     * @return 实体集合
+     */
+    List<T> list(Filter... filters);
+
+    /**
+     * 依据filter查询实体集合
+     *
+     * @param first   起始记录
+     * @param count   数量
+     * @param filters 筛选
+     * @param orders  排序
+     * @return 实体集合
+     */
+    List<T> list(Integer first, Integer count, List<Filter> filters, List<Order> orders);
 
 }
